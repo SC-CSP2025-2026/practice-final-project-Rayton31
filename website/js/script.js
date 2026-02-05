044a11fc0035fa0c47bcbcbb01349c6d92f272ea
Rayton Shi <Ruitong.Shi@scsstudent.org>
2026-02-05 12:14:39 -0800

    feat: COmplete video 1

--- website/js/script.js
+++ website/js/script.js
@@ -1,5 +1,6 @@
-const url =
-  "https://student-api-proxy.onrender.com/api/basketball-head.p.rapidapi.com/players/jordami01/stats/PerGame?seasonType=Regular";
+const loadRosterByTeam = async (teamId) => {
+  const url =
+  `https://student-api-proxy.onrender.com/api/basketball-head.p.rapidapi.com/teams/${teamId}/roster/2016-2017`;
 const options = {
   method: "GET",
   headers: {
@@ -8,14 +9,38 @@ const options = {
   },
 };
 
-fetch(url, options)
-  .then((response) =>
-    response.json().then((result) => {
-      console.log(result.data); // Your API data
-      console.log(`Cost: $${result.meta.cost}`);
-      console.log(`Remaining: $${result.meta.remaining_budget}`);
-    }),
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+const response = await fetch (url, options);
+const result = await response.json();
+const data = result.data; // Your API response
+
+console.log(data);
+
+}
+  
+  
+  const gswButton = document.querySelector("#GSW-btn");
+  const houButton = document.querySelector("#HOU-btn");
+  const lalButton = document.querySelector("#LAL-btn");
+
+  console.log(lalButton);
+  console.log(gswButton);
+  console.log(houButton);
+
+
+const listGroup = document.querySelector(".list-group");
+
+loadRosterByTeam("general");
+
+
+// fetch(url, options)
+//   .then((response) =>
+//     response.json().then((result) => {
+//       console.log(result.data); // Your API data
+//       console.log(`Cost: $${result.meta.cost}`);
+//       console.log(`Remaining: $${result.meta.remaining_budget}`);
+//     }),
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
